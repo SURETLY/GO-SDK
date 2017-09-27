7ae7b7c6cc0936e0a9b2ae6deaba538be4271530
Andrey Sanatullov <mr.monty.jo@gmail.com>
2017-09-27 12:15:11 +0700

    create base methods

---
+++ sdk.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"net/http"
+)
+
+func init() {
+
+}
+
+type Suretly struct {
+	client http.Client
+	Id string
+	Token string
+	Order order
+}
+
+type order struct {}
+
+// common
+func (s Suretly) Options()  {
+
+}
+
+func (s Suretly) Orders()  {
+
+}
+
+// create order and actions with orders
+func (s Suretly) OrderNew()  {
+
+}
+
+func (s Suretly) OrderStatus()  {
+
+}
+
+func (s Suretly) OrderStop()  {
+
+}
+
+func (s Suretly) OrderIssued()  {
+	
+}
+
+func (s Suretly) OrderPaid()  {
+	
+}
+
+func (s Suretly) OrderPartialPaid()  {
+	
+}
+
+func (s Suretly) OrderUnpaid()  {
+	
+}
+
+func (s Suretly) ContractGet()  {
+	
+}
+
+func (s Suretly) ContractAccept()  {
+	
+}
+
+func (s Suretly) Currencies() () {
+
+}
+
+func (s Suretly) Countries() () {
+
+}
